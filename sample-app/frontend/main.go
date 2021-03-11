461b3d0c75b021b5f5733f45dc4d584cd64d3ee5
Caio Marte <caiomartesilva@gmail.com>
2021-03-10 22:50:57 -0300

    First go html server

---
+++ sample-app/frontend/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<div>%s</div>", p.Body)
+}
+
+func loadPage(title string) (*Page, error) {
+	filepath := title + "/" + title + ".html"
+	body, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	http.HandleFunc("/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// REFERENCES
+// https://golang.org/doc/articles/wiki/
